304309674d14ad5cf93e046c316b6e7736184162
MaxVein <58347366+MaxVein@users.noreply.github.com>
2020-11-09 14:18:53 +0200

    Added angular material

--- dtApi/src/app/app.module.ts
+++ dtApi/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { AngularMaterialModule } from './shared/modules/angular-material/angular-material.module'
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, AngularMaterialModule],
     providers: [],
     bootstrap: [AppComponent],
 })
